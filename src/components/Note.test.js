af14f74fdcc5d0e6f52b0b38b9783e778d2c1ffe
sjq1412 <sarahdev.cod3@gmail.com>
2023-09-16 16:50:22 +0800

    Note test - clicking button to make note not important with user-event

--- src/components/Note.test.js
+++ src/components/Note.test.js
@@ -1,5 +1,6 @@
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Note from './Note'
 
 describe('Note component', () => {
@@ -9,7 +10,21 @@ describe('Note component', () => {
 
     const div = container.querySelector('.note')
 
-    screen.debug(div)
+    // screen.debug(div)
     expect(div).toHaveTextContent('this is a note')
+  })
+
+  test('clicking the button calls event handler once', async () => {
+    const note = { content: 'this is a note', important: true }
+
+    const mockHandler = jest.fn()
+
+    render(<Note note={note} toggleNoteImportance={mockHandler} />)
+
+    const user = userEvent.setup()
+    const button = screen.getByText('make not important')
+    await user.click(button)
+
+    expect(mockHandler.mock.calls).toHaveLength(1)
   })
 })
