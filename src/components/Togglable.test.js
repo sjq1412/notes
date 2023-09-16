4913779f39b9737163fe922c93390a69feb42169
sjq1412 <sarahdev.cod3@gmail.com>
2023-09-16 17:05:40 +0800

    test Togglable component

---
+++ src/components/Togglable.test.js
@@ -0,0 +1,36 @@
+import '@testing-library/jest-dom'
+import { screen, render } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import Togglable from './Togglable'
+
+describe('Togglable component', () => {
+  let container
+
+  beforeEach(() => {
+    container = render(
+      <Togglable buttonLabel='show...'>
+        <div className='testDiv'>
+            togglable content
+        </div>
+      </Togglable>
+    ).container
+  })
+
+  test('renders its children', async () => {
+    await screen.findAllByText('togglable content')
+  })
+
+  test('at start the children are not displayed', () => {
+    const div = container.querySelector('.togglableContent')
+    expect(div).toHaveStyle('display: none')
+  })
+
+  test('after clicking the button, children are displayed', async () => {
+    const user = userEvent.setup()
+    const button = screen.getByText('show...')
+    await user.click(button)
+
+    const div = container.querySelector('.togglableContent')
+    expect(div).not.toHaveStyle('display: none')
+  })
+})
